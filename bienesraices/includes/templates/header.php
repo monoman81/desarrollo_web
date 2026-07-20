05d4a2f42e37eac23cce3b3d1f55e4925b1222e0
Carlos Rene Ponce Novelo <crpn81@yahoo.com.mx>
2026-07-19 22:25:33 -0600

    Bienes Raices. Agregado PHP al proyecto.
    Ya se preparo el proyecto para usar php.

---
+++ bienesraices/includes/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+    <header class="header <?php echo isset($inicio) && $inicio ? "inicio" : "" ?>">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="index.php">
+                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                </a>
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="Icono menu">
+                </div>
+
+                <div class="derecha">
+                    <img src="build/img/dark-mode.svg" alt="dark mode" class="dark-mode-boton">
+                    <nav class="navegacion">
+                        <a href="nosotros.php">Nosotros</a>
+                        <a href="anuncios.php">Anuncios</a>
+                        <a href="blog.php">Blog</a>
+                        <a href="contacto.php">Contacto</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
--- bienesraices/index.php
+++ bienesraices/index.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="index.html">
-                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
-                </a>
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="Icono menu">
-                </div>
-
-                <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="dark mode" class="dark-mode-boton">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-                
-            </div>
-            <h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>
-        </div>
-    </header>
+<?php 
+    $inicio = true;
+    include("includes/templates/header.php"); 
+?>
     <main class="contenedor seccion">
         <h1>Mas Sobre Nosotros</h1>
         <div class="iconos-nosotros">
@@ -228,18 +199,4 @@
         </section>
     </div>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-        <p class="copyright">Todos los derechos reservados 2021 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php include("includes/templates/footer.php") ?>
